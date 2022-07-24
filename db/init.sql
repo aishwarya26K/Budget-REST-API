409c3726c332f96cc25dbf6ae7ebffe012b7a06a
aishwarya26k <aishwaryasingh16k@gmail.com>
2022-07-24 23:56:16 +0530

    Fixed the mysql connection for docker compose

--- db/init.sql
+++ db/init.sql
@@ -14,24 +14,46 @@ CREATE TABLE `budgets` (
   `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+
+--
+-- Table structure for table `categories`
+--
+
 CREATE TABLE `categories` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `income_expenses`
+--
+
 CREATE TABLE `income_expenses` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `amount` double NOT NULL,
   `budget_id` int(11) NOT NULL,
   `is_expense` tinyint(1) DEFAULT NULL,
-  `category_id` int(11) NOT NULL
+  `category_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `income_expenses`
+--
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
@@ -42,47 +64,91 @@ CREATE TABLE `users` (
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `users`
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `budgets`
+--
 ALTER TABLE `budgets`
   ADD PRIMARY KEY (`id`),
   ADD KEY `user_id` (`user_id`);
 
-
+--
+-- Indexes for table `categories`
+--
 ALTER TABLE `categories`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
 
-
+--
+-- Indexes for table `income_expenses`
+--
 ALTER TABLE `income_expenses`
   ADD PRIMARY KEY (`id`),
   ADD KEY `budget_id` (`budget_id`),
-  ADD KEY `category_id` (`category_id`);
+  ADD KEY `category_id` (`category_id`),
+  ADD KEY `user_id` (`user_id`);
 
-
+--
+-- Indexes for table `users`
+--
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `email` (`email`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
 
+--
+-- AUTO_INCREMENT for table `budgets`
+--
 ALTER TABLE `budgets`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
-
-ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
-
-ALTER TABLE `income_expenses`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
-
-ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
+--
+-- AUTO_INCREMENT for table `income_expenses`
+--
+ALTER TABLE `income_expenses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `budgets`
+--
 ALTER TABLE `budgets`
   ADD CONSTRAINT `budgets_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
+--
+-- Constraints for table `categories`
+--
+ALTER TABLE `categories`
+  ADD CONSTRAINT `categories_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
+--
+-- Constraints for table `income_expenses`
+--
 ALTER TABLE `income_expenses`
   ADD CONSTRAINT `income_expenses_ibfk_1` FOREIGN KEY (`budget_id`) REFERENCES `budgets` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `income_expenses_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-COMMIT;
+  ADD CONSTRAINT `income_expenses_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `income_expenses_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
--- src/utils/connection.py
+++ src/utils/connection.py
@@ -3,17 +3,27 @@ import os
 
 def getConnectToSQLdb():
     mydb = None
+    UNIX_SOCKET = os.getenv("MYSQL_UNIX_SOCKET")
     try:
-        mydb = mysql.connector.connect(
-        host=os.getenv("MYSQL_HOST"),
-        username=os.getenv("MYSQL_USER_NAME"),
-        password=os.getenv("MYSQL_PASSWORD"),
-        database=os.getenv("MYSQL_DB"),
-        unix_socket=os.getenv("MYSQL_UNIX_SOCKET")
-        )
+        if UNIX_SOCKET:
+            mydb = mysql.connector.connect(
+                host=os.getenv("MYSQL_HOST"),
+                username=os.getenv("MYSQL_USER_NAME"),
+                password=os.getenv("MYSQL_PASSWORD"),
+                database=os.getenv("MYSQL_DB"),
+                unix_socket=UNIX_SOCKET
+            )
+        else:
+            mydb = mysql.connector.connect(
+                host=os.getenv("MYSQL_HOST"),
+                username=os.getenv("MYSQL_USER_NAME"),
+                password=os.getenv("MYSQL_PASSWORD"),
+                database=os.getenv("MYSQL_DB"),
+            )
 
         return mydb
     except Exception as e:
         print(str(e))
-        mydb.close()
-
+        if mydb:
+            mydb.close()
+            
